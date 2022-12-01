cad1b70953396dbbef220419b8ed75c6e50b5a40
Valov Ivan <pechenkooo3@gmail.com>
2022-12-01 21:15:42 +0200

    11

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  buttonStop.disabled = true;
+  buttonStart.disabled = false;
+});

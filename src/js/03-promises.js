1b661ce3bf6b2111b463fa26a19021ff1df08910
Valov Ivan <pechenkooo3@gmail.com>
2022-12-03 22:41:42 +0200

    31

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,34 @@
 const form = document.querySelector('.form');
-form.addEventListener('submit', onFormSubmit)
+form.addEventListener('submit', onFormSubmit);
 
 function createPromise(position, delay) {
-  return new Promise((resolve, rject) => {
+  return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
-     if (shouldResolve) {
-      resolve({position, delay})
-       
-     } else {
-       rject({position, delay})
-    }
-  },delay)
-  })
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
- 
 
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const formEl = evt.currentTarget.elements;
+  let delay = Number(formEl.delay.value);
+  const delayStep = Number(formEl.step.value);
+  const amount = Number(formEl.amount.value);
+
+  for (let i = 1; i < amount; i += 1) {
+    delay += delayStep;
+    createPromise(2, 1500)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}

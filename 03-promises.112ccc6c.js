6bc1060204feaed390f8e7fe28a34d4b44f5d84d
Pechenkooo3 <106810267+Pechenkooo3@users.noreply.github.com>
2022-12-03 20:30:55 +0000

    Deploying to gh-pages from @ Pechenkooo3/goit-js-hw-09@ed6ca7cf7573ae994937cbc679ca1a38b040594f 🚀

---
+++ 03-promises.112ccc6c.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",onFormSubmit);
+//# sourceMappingURL=03-promises.112ccc6c.js.map
---
+++ 03-promises.bee5a6ee.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",onFormSubmit);
+//# sourceMappingURL=03-promises.bee5a6ee.js.map
